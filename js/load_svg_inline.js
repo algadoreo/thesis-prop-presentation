5d54d5779fa816a0eb0c687dd34b44c00fdec16a
Jason Leung <leung@astro.utoronto.ca>
2019-02-03 20:10:43 -0500

    Initial commit

---
+++ js/load_svg_inline.js
@@ -0,0 +1,35 @@
+$(function(){
+  $('#emodes').load("img/pol_emodes.svg");
+  $('#bmodes').load("img/pol_bmodes.svg");
+
+  $('#spider_bmodes_axes').load("img/spider_bmodes_axes.svg");
+  $('#spider_bmodes_axesnovertaxis').load("img/spider_bmodes_axesnovertaxis.svg");
+  $('#spider_bmodes_bbprimp03').load("img/spider_bmodes_bbprimp03.svg");
+  $('#spider_bmodes_bbprimp06').load("img/spider_bmodes_bbprimp06.svg");
+  $('#spider_bmodes_bbtotal').load("img/spider_bmodes_bbtotal.svg");
+  $('#spider_bmodes_axesvertaxis').load("img/spider_bmodes_axesvertaxis.svg");
+  $('#spider_bmodes_lensing, #spider_bmodes_lensing2').load("img/spider_bmodes_lensing.svg");
+  $('#spider_bmodes_planck, #spider_bmodes_planck2').load("img/spider_bmodes_planck.svg");
+  $('#spider_bmodes_bicep2keck, #spider_bmodes_bicep2keck2').load("img/spider_bmodes_bicep2keck.svg");
+  $('#spider_bmodes_spider').load("img/spider_bmodes_spider.svg");
+
+  $('#spider_bands_axes').load("img/spider_bands_axes.svg");
+  $('#spider_bands_atm').load("img/spider_bands_atm.svg");
+  $('#spider_bands_atm35').load("img/spider_bands_atm35.svg");
+  $('#spider_bands_co').load("img/spider_bands_co.svg");
+  $('#spider_bands_bands').load("img/spider_bands_bands.svg");
+
+  $('#spider_field_bg').load("img/spider_field_bg.svg");
+  $('#spider_field_field').load("img/spider_field_field.svg");
+
+  $('#foregrounds_axes').load("img/cmb_foregrounds_axes.svg");
+  $('#foregrounds_cmb').load("img/cmb_foregrounds_cmb.svg");
+  $('#foregrounds_dust').load("img/cmb_foregrounds_dust.svg");
+  $('#foregrounds_sync').load("img/cmb_foregrounds_sync.svg");
+  $('#foregrounds_spider').load("img/cmb_foregrounds_spider.svg");
+
+  $('#spider_line_white').load("img/spider_line_white_simplified.svg");
+  $('#spider_line_white_small').load("img/spider_line_white_simplified.svg");
+
+  $('#ssn').load("img/ssn.svg");
+});
